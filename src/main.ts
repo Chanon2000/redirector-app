e07851c926ff5be5d9964458746fa2edb0a8332f
Chanon <chanonmixmay2543@hotmail.com>
2025-09-21 08:17:33 +0700

    Refactor application structure and add server-side rendering support

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { App } from './modules/app/app';
+import { appConfig } from './modules/app/app.config';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
