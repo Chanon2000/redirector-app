82f053d34ca1cfe6529c2174445f8fb622cf21fb
Chanon <chanonmixmay2543@hotmail.com>
2025-09-22 17:07:47 +0700

    Refactor routing and component initialization; comment out unused router outlet

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('redirector-app');
+
+  constructor() {
+    console.log('App component initialized');
+    // console.log('Current URL:', window.location.href);
+    // console.log('Current Path:', window.location.pathname);
+    const pathMatch = window.location.pathname.match(/^\/([^/?]+)$/);
+    const query: URLSearchParams = new URLSearchParams(window.location.search);
+
+    if (pathMatch) {
+      const protocol = pathMatch[1];
+      const authority = Array.from(query.keys())[0];
+      const value = query.get(authority || '');
+
+      if (protocol && authority && value) {
+        const redirectUrl = `${protocol}://${authority}${value}`;
+        window.location.href = redirectUrl;
+      }
+    }
+  }
 }
