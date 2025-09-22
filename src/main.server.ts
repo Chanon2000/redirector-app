a257099334cd26dee7794328d618b25be17d48d6
Chanon <chanonmixmay2543@hotmail.com>
2025-09-22 14:39:32 +0700

    Remove SSR

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { BootstrapContext, bootstrapApplication } from '@angular/platform-browser';
-import { App } from './modules/app/app';
-import { config } from './modules/app/app.config.server';
-
-const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
-
-export default bootstrap;
